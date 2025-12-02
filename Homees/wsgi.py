156c9f5446acbc841204b0af6bb97058a381689b
pruthvimkadri <pruthvim943@gmail.com>
2025-12-02 12:40:03 +0530

    Update wsgi.py

--- Homees/wsgi.py
+++ Homees/wsgi.py
@@ -1,4 +1,5 @@
-from main import app   # import the Flask app object from main.py
+from Homees.main import app   # import the Flask app object from main.py
 
 if __name__ == "__main__":
     app.run()
+
